1cca4ba295a7a4875b07363b959136c533528d6f
Christian Wilcox <christian.wilcox@etcconnect.com>
2011-03-31 11:17:37 -0500

    added contents of generated Addon from https://addons.mozilla.org/en-US/firefox/developers/tools/builder

---
+++ xpi/defaults/preferences/prefs.js
@@ -0,0 +1,6 @@
+pref("extensions.writefile.boolpref", false);
+pref("extensions.writefile.intpref", 0);
+pref("extensions.writefile.stringpref", "A string");
+
+// https://developer.mozilla.org/en/Localizing_extension_descriptions
+pref("extensions.cw@christianwilcox.com.description", "chrome://writefile/locale/overlay.properties");
